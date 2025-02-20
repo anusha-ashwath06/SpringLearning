4800fe466df1b94b0811b73fe367e6dcb30cdbe4
anusha-ashwath06 <anushaa.21.beis@acharya.ac.in>
2025-02-20 18:35:24 +0530

    new commit

--- todo-app/src/components/counter/counter.js
+++ todo-app/src/components/counter/counter.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { PropTypes } from 'prop-types'
 import './Counter.css'
 import CounterButton from './CounterButton'
-
+//sample comment
 export default function Counter() {
 
   const [count, setCount] = useState(0)
